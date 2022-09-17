14229a15d1c6986cd6d7799b4742e33a3e6d3780
무이 <87917696+MuiSara@users.noreply.github.com>
2022-09-17 20:37:49 +0900

    Add files via upload

---
+++ SkyApi.js
@@ -0,0 +1,104 @@
+const express = require('express')
+const uuidAPIKey = require('uuid-apikey')
+const ciqlJson = require('ciql-json')
+const bodyParser = require('body-parser')
+const userSky = require('./Data/Sky.json')
+const buyList = require('./Data/buyList.json')
+const app = express();
+const key = {
+    apiKey: '1Z07MBH-PSD4P1A-HEHN65Q-MGPY0KT',
+    uuid: '0fc07a2e-b65a-4b05-8ba3-5316a42de04f'
+};
+
+const server = app.listen(3001, () => {
+    console.log("Start Server : localhost:3001")
+});
+
+console.log(uuidAPIKey.create())
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.get('/', async (req, res) => {
+    res.sendStatus(403)
+})
+
+app.get('/api/server/ping/:apikey', async (req, res) => {
+    let {
+        apikey
+    } = req.params;
+
+    try {
+        if (!uuidAPIKey.check(apikey, key.uuid)) {
+            res.send('<body style="background-color:black;"> <p style="color:white;">Check, Your API Key!</p> </body>')
+        } else {
+            res.send('<body style="background-color:black;"> <p style="color:white;">Pong!</p> </body>')
+
+        }
+    } catch {
+        res.send('<body style="background-color:black;"> <p style="color:white;">Check, Your API Key!</p> </body>')
+    }
+
+})
+
+app.get('/api/sky/:userID/get/:apiKey/', async (req, res) => {
+    let {
+        userID,
+        apiKey
+    } = req.params;
+
+    try {
+        if (!uuidAPIKey.check(apiKey, key.uuid)) {
+            res.send('<body style="background-color:black;"> <p style="color:white;">Check, Your API Key!</p> </body>')
+        } else {
+            res.send(String(userSky[userID]))
+        }
+    } catch {
+        res.send('<body style="background-color:black;"> <p style="color:white;">Check, Your API Key!</p> </body>')
+    }
+
+})
+
+app.get('/api/sky/:userID/add/:sky/:apiKey/', async (req, res) => {
+    let {
+        userID,
+        sky,
+        apiKey
+    } = req.params;
+
+    try {
+        if (!uuidAPIKey.check(apiKey, key.uuid)) {
+            res.send('<body style="background-color:black;"> <p style="color:white;">Check, Your API Key!</p> </body>')
+        } else {
+            ciqlJson
+                .open("Data/Sky.json")
+                .set(userID, parseInt(userSky[userID]) + parseInt(sky))
+                .save()
+            res.send(String(userSky[userID]))
+        }
+    } catch {
+        res.send('<body style="background-color:black;"> <p style="color:white;">Check, Your API Key!</p> </body>')
+    }
+
+})
+
+app.get('/api/sky/:userID/set/:sky/:apiKey/', async (req, res) => {
+    let {
+        userID,
+        sky,
+        apiKey
+    } = req.params;
+
+    try {
+        if (!uuidAPIKey.check(apiKey, key.uuid)) {
+            res.send('<body style="background-color:black;"> <p style="color:white;">Check, Your API Key!</p> </body>')
+        } else {
+            ciqlJson
+                .open("Data/Sky.json")
+                .set(userID, parseInt(sky))
+                .save()
+            res.send(String(userSky[userID]))
+        }
+    } catch {
+        res.send('<body style="background-color:black;"> <p style="color:white;">Check, Your API Key!</p> </body>')
+    }
+
+})
